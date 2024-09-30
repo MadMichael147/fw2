9208297bba156e47111e87936904c4c980f7ea87
Michael <116743118+MadMichael147@users.noreply.github.com>
2024-09-30 14:15:24 +0000

    Aula 30-09

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,24 @@
 import Footer from "./modulos/footer";
 import Navbar from "./modulos/navbar";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 export default function Index() {
   return<div>
     <Navbar/>
       Bem Vindos
+        <Container>
+          <Row>
+            <Col className="bg-secondary">1 of 2</Col>
+            <Col className="bg-danger-subtle">2 of 2</Col>
+          </Row>
+          <Row>
+            <Col className="bg-warning-subtle">1 of 3</Col>
+            <Col className="bg-success-subtle">2 of 3</Col>
+            <Col className="bg-dark-subtle">3 of 3</Col>
+          </Row>
+        </Container>
     <Footer/>
     
   </div>;
